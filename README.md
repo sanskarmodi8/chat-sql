c0cb8be3ae6692e64d90d4551803166899de58ac
Sanskar Modi <sansyprog8@gmail.com>
2024-08-12 18:54:36 +0530

    completed project

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# chat-sql
+# 🦜 Chat with SQL Database
+
+This project is a Streamlit application that allows users to interact with a MySQL database through a conversational interface powered by the ChatGroq language model. It utilizes LangChain tools to create an SQL agent capable of running queries and retrieving data based on user input.
+
+## Key Technologies Used
+
+- **Streamlit:** Web framework for creating interactive web applications in Python.
+- **LangChain:** Framework for developing applications powered by large language models (LLMs).
+- **Groq Api:** Language model used for generating responses to natural language queries.
+
+## Setup Instructions
+
+### Prerequisites
+
+- Python 3.8+
+- MySQL database with necessary credentials
+- API key for ChatGroq (stored in a `.env` file)
+
+### Installation
+
+1. **Clone the repository:**
+
+    ```bash
+    git clone https://github.com/sanskarmodi8/chat-sql.git
+    cd chat-sql
+    ```
+
+2. **Create a virtual environment and activate it:**
+
+    ```bash
+    python3 -m venv chatsqlvenv
+    source chatsqlvenv/bin/activate
+    ```
+
+3. **Install dependencies:**
+
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4. **Set up environment variables:**
+
+    Create a `.env` file in the root directory of the project and add your ChatGroq API key:
+
+    ```
+    GROQ_API_KEY=your_api_key_here
+    ```
+
+5. **Run the application:**
+
+    ```bash
+    streamlit run app.py
+    ```
+
+## Usage
+
+1. **Provide MySQL connection details:**
+   - Enter the MySQL host, user, password, and database name in the sidebar.
+
+2. **Start a conversation:**
+   - Use the chat interface to input clear and concise natural language queries related to your MySQL database.
+
+3. **Review Results:**
+   - The application will process your queries using the ChatGroq model and return the results from the database.
+
+## Contributing
+
+Contributions are welcome! If you have ideas for improvements or new features, feel free to fork the repository and submit a pull request.
+
+## Contact
+
+If you have any questions or need further assistance, please reach out to [sansyprog8@gmail.com](mailto:sansyprog8@gmail.com).
---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.agents import create_sql_agent
+from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from sqlalchemy import create_engine
+import sqlite3
+from langchain_groq import ChatGroq
+import os
+
+#get the groq api key
+load_dotenv()
+key = os.getenv('GROQ_API_KEY')
+
+st.set_page_config(page_title="Chat with SQL DB", page_icon="🦜")
+st.title("🦜 Chat with SQL DB")
+
+# Display a warning about SQL injection
+st.warning(" ⚠️ Be cautious of SQL injection risks when interacting with databases.")
+
+mysql_host = st.sidebar.text_input("Provide MySQL Host")
+mysql_user = st.sidebar.text_input("MySQL User")
+mysql_password = st.sidebar.text_input("MySQL password", type="password")
+mysql_db = st.sidebar.text_input("MySQL Database")
+
+#llm
+llm = ChatGroq(groq_api_key = key, model_name="Llama3-8b-8192", streaming=True)
+
+@st.cache_resource(ttl="2h")
+def configure_db(host=None, user=None, password=None, db=None):
+	if not (host and user and password and db):
+		st.error("Please provide all MySQL connection details.")
+		st.stop()
+	return SQLDatabase(create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{db}"))
+
+#configure db
+db = configure_db(mysql_host, mysql_user, mysql_password, mysql_db)
+
+#toolkit
+toolkit = SQLDatabaseToolkit(db=db,llm=llm)
+
+agent = create_sql_agent(
+	llm=llm,
+	toolkit=toolkit,
+	verbose=True,
+	agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+	handle_parsing_errors=True
+)
+
+if "messages" not in st.session_state or st.sidebar.button("Clear message history"):
+	st.session_state["messages"]=[{"role":"assistant", "content":"How can I help you?"}]
+
+for msg in st.session_state.messages:
+	st.chat_message(msg["role"]).write(msg["content"])
+
+if query:=st.chat_input(placeholder="Ask anything from the database"):
+	st.session_state.messages.append({"role":"user", "content":query})
+	st.chat_message("user").write(query)
+	with st.chat_message("assistant"):
+#		display the thinking of the agent
+#		st_cb = StreamlitCallbackHandler(st.container())
+#		res = agent.run(query, callbacks=[st_cb])
+		res = agent.run(query)
+		st.session_state.messages.append({"role":"assistant", "content":res})
+		st.write(res)
+
+
